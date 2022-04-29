0220bbe6a17f0e47b4171231720601387e109263
Donatella7 <campaniellodonatella@gmail.com>
2022-04-29 15:34:18 +0200

    modifiche jooin

--- query_join.php
+++ query_join.php
@@ -2,21 +2,21 @@
 
     SELECT `students`.`name`,`students`.`surname`, `degrees`.`name` AS `degree_name`
     FROM `degrees`
-    JOIN `students`
+    RIGHT JOIN `students`
     ON `students`.`degree_id` = `students`.`id`
     WHERE `degrees`.`name` = "Corso di Laurea in Economia";
 
 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze:
     SELECT `departments`.`name` AS `department` , `degrees`.`name` AS `degree_name`
     FROM `departments`
-    JOIN  `degrees`
+    LEFT JOIN  `degrees`
     ON  `degrees`.`department_id` = `departments`.`id`
     WHERE `departments`.`name` = "Dipartimento di Neuroscienze";
 
 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44) :  
     SELECT DISTINCT `courses`.`id` AS  `course_id`, `courses`.`name` AS  `course_name` ,`teachers`.`name` AS `teacher_name` , `teachers`.`surname` AS `teacher_surname`
     FROM `courses`
-    JOIN `course_teacher`
+    LEFT JOIN `course_teacher`
     ON 	`course_teacher`.`teacher_id` =  `courses`. `id`
 
     JOIN `teachers`
