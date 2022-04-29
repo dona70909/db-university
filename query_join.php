0e2f405adb3c9de9893b109761df40be5092cc5a
Donatella7 <campaniellodonatella@gmail.com>
2022-04-29 15:06:51 +0200

    2nd query done

---
+++ query_join.php
@@ -0,0 +1,17 @@
+1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia:
+
+    SELECT `students`.`name`,`students`.`surname`, `degrees`.`name` AS `degree_name`
+    FROM `degrees`
+    JOIN `students`
+    ON `students`.`degree_id` = `students`.`id`
+    WHERE `degrees`.`name` = "Corso di Laurea in Economia";
+
+2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze:
+    SELECT `departments`.`name` AS `department` , `degrees`.`name` AS `degree_name`
+    FROM `departments`
+    JOIN  `degrees`
+    ON  `degrees`.`id` = `departments`.`id`
+    WHERE `departments`.`name` = "Dipartimento di Neuroscienze";
+
+3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44) :  
+    
